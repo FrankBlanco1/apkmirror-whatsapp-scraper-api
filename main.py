f1d8e8e72aad9fd96e2ecb4363e0b792cbdaee6b
FrankABlanco <frankbg.000914@gmail.com>
2024-03-27 16:10:27 -0400

    Make a function to keep the servoce running on render always alive

--- main.py
+++ main.py
@@ -3,7 +3,31 @@ from fastapi import FastAPI
 from fastapi_utilities import repeat_every
 from apkMirrorScrapper import get_WhatsApp_metadata
 from contextlib import asynccontextmanager
+import requests
 import json
+
+@repeat_every(seconds=60*10)
+async def keep_service_alive():
+    
+    # This function is for make sure that service wont down because render free plan
+    # (If the service is innactive for 15 minutes it will down)
+    
+    try:
+        r = requests.get("https://waupdate.onrender.com")
+        
+        if r.status_code == 503:
+            print("Warning: Service Unavailable")
+        
+        if r.status_code == 502:
+            print("Warning: Bad Gateway")
+        
+        if r.status_code == 200:
+            print("Info: Service is alive")
+    
+    except Exception as e:
+    
+        print(str(e))     
+    
 
 # Execute on startup (see lifespan) and every 24 hours
 @repeat_every(seconds=60 * 60 * 24) 
@@ -27,6 +51,7 @@ async def get_apkmirror_data():
 @asynccontextmanager
 async def lifespan(app : FastAPI):
     await get_apkmirror_data()
+    await keep_service_alive()
     yield
     
 app = FastAPI(lifespan=lifespan)
